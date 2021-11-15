62cf23505b80744f607711ffacfa081d4579f681
Eric Esteban <eric_7@hotmail.com>
2021-11-15 10:54:10 +0100

    done

--- seeds/movies.seed.js
+++ seeds/movies.seed.js
@@ -85,24 +85,7 @@ const Movie = require("../models/Movie.model");
 
 const mongoose = require("mongoose");
 
-const MONGO_URI =
-  process.env.MONGODB_URI || "mongodb://localhost/lab-express-cinema";
-
-mongoose
-  .connect(MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-    useCreateIndex: true,
-  })
-  .then((x) => {
-    console.log(
-      `Connected to Mongo! Database name: "${x.connections[0].name}"`
-    );
-    Movie.insertMany(movies).then(() => {
-      console.log("all good");
-    });
-  })
-  .catch((err) => {
-    console.error("Error connecting to mongo: ", err);
-  });
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
