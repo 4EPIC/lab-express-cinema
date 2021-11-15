b1d21bd4de1fe41b36bcebadf1aabfcce3e1e7a7
Eric Esteban <eric_7@hotmail.com>
2021-11-15 09:17:26 +0100

    done

---
+++ routes/movies.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const router = express.Router();
+const Movie = require("../models/Movie.model.js");
+
+router.get("/", (req, res, next) => {
+  Movie.find()
+    .then((allMoviesFromDataBase) => {
+      console.log(allMoviesFromDataBase);
+      res.render("movies", { movies: allMoviesFromDataBase });
+    })
+    .catch((error) => {
+      console.log("Error while getting the movies from the DB:", error);
+      next(error);
+    });
+});
+
+router.get("/:id", (req, res, next) => {
+  Movie.findById(req.params.id)
+    .then((oneMovie) => {
+      res.render("movie-details", { movies: oneMovie });
+    })
+    .catch((error) => {
+      console.log("Error while getting the movies from DB:", error);
+      next(error);
+    });
+});
+
+module.exports = router;
